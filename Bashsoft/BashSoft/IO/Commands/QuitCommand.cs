bfac1d230ce363f88c9a4149bf50b336c7deb334
eto1302 <martinangelov1302@gmail.com>
2018-04-18 14:17:15 +0300

    This is the quit command for the Bashsoft project

---
+++ Bashsoft/BashSoft/IO/Commands/QuitCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BashSoft.Attributes;
+using BashSoft.Contracts;
+using BashSoft.Exceptions;
+
+namespace BashSoft.IO.Commands
+{
+    [Alias("quit")]
+    class QuitCommand : Command
+    {
+        
+        public QuitCommand(string input, string[] data) : base(input, data)
+
+        {
+
+        }
+
+        public override void Execute()
+        {
+            if (this.Data.Length != 1)
+            {
+                throw new InvalidCommandException(this.Input);
+            }
+            Environment.Exit(0);
+        }
+    }
+}
